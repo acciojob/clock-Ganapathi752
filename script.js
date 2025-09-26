da1e353f8f7d8dea96432b08b01bbdb47c04c807
Ganapathi752 <gana.ganapathi2003@gmail.com>
2025-09-26 14:26:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+//html
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+	<!-- the entire body must be written by student -->
+	<p id="timer"></p>
+    <script type="text/javascript" src="./script.js">
+</script>
+
+</body>
+
+</html>
+
+
+//js
+function renderDate() {
+	const date = new Date()
+	document.getElementById("timer").innerHTML = date.toLocaleString().toUpperCase()
+}
+
+setInterval(() => {
+	renderDate()
+})
